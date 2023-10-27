3a03fceca64fb1845b57d876cac8028e67d3ee16
Valentina <valentina@iridian.co>
2023-10-27 00:25:41 -0500

    Propuesta rector

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Detectar el evento de scroll
+window.addEventListener('scroll', function () {
+  var header = document.getElementById('header');
+
+  // Cuando el usuario hace scroll, verifica la posición del scroll
+  if (window.scrollY > 50) {
+      header.classList.add('white');
+  } else {
+      header.classList.remove('white'); 
+  }
+});
