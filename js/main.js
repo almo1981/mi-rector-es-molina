b9bf12459a3f981c4471d4caa658c8c8f3095036
Valentina <valentina@iridian.co>
2023-11-06 06:45:23 -0500

    Ajuste hoja de vida y rotador de imagenes

--- js/main.js
+++ js/main.js
@@ -26,3 +26,29 @@ menuIcon.addEventListener("click", () => {
 closeIcon.addEventListener("click", ()=>{
   sideMenu.classList.remove("open");
 });
+
+
+//swiper
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
